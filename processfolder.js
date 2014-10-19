2f05c6627b0ff8571b2d65628b500c84871511e0
Andrew Trautmann <atraut93@gmail.com>
2014-10-18 23:13:44 -0400

    Created a program to process all MusicXML files in a specified folder.

---
+++ processfolder.js
@@ -0,0 +1,54 @@
+var xml2json = require('xml-to-json'),
+    fs = require('fs');
+    Score = require('./score'),
+    folder = process.argv[2];
+
+var processFolder = function (currDir, currObj) {
+    var filePath, fileStats;
+    if (currDir) {
+        filePath = currDir + '/' + currObj;
+    } else {
+        filePath = currObj;
+    }
+    var fileStats = fs.statSync(filePath);
+    if (fileStats.isFile()) {
+        processFile(currDir, currObj);
+    } else if (fileStats.isDirectory) {
+        fs.readdir(filePath, function (err, files) {
+            if (err) {
+                console.error('Error processing folder:' + err);
+                return;
+            } else {
+                files.forEach(function (file) {
+                    processFolder(filePath, file);
+                });
+            }
+        });
+    }
+}
+
+var processFile = function (currDir, currFile) {
+    var filePath;
+    if (currDir) {
+        filePath = currDir + '/' + currFile;
+    } else {
+        filePath = currFile;
+    }
+    if (filePath.indexOf('.xml') == -1) {
+        return;
+    }
+    xml2json({
+        input: filePath,
+        output: null
+    }, function (err, json) {
+        if (err) {
+            console.error('Error parsing xml: ' + err);
+        } else {
+            console.log(currFile);
+            var s = new Score(json);
+            s.process();
+        }
+    });
+}
+
+processFolder(null, folder);
