9473a6b0fd9f2ccf977cdfc736a5ca4f4232555d
Ben Burwell <bburwell1@gmail.com>
2014-11-24 22:20:46 -0500

    Add compressed MXL functionality and bump to 0.1.1

--- examples/processFile.js
+++ examples/processFile.js
@@ -1,5 +1,11 @@
 var music = require('../lib/index.js');
 
+music.parseMXLFile('Chariots_of_Fire.mxl', function(score) {
+    score.parts.forEach(function(part) {
+        console.log(part.getRawStats());
+    });
+});
+
 music.parseMusicXMLFile('testScore.xml', function(score) {
     score.parts.forEach(function(part) {
         console.log(part.getRawStats());
